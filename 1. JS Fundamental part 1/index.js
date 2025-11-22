b15047e1f278de21c14531bcc998279955fb92ee
aditya <89adityapandey@gmail.com>
2025-11-22 20:41:15 +0530

    changes

---
+++ 1. JS Fundamental part 1/index.js
@@ -0,0 +1,36 @@
+// const userProfile = {
+//     name:"ansh pandey",
+//     address:"sujakhar",
+// }
+
+
+const userProfile = [
+    {
+    name:"ansh pandey",
+    address:"sujakhar",
+    },
+    {
+    name:"ansh  4pandey",
+    address:"sujakhar",
+    },
+    {
+    name:"ansh2 pandey",
+    address:"sujakhar",
+    },
+    {
+    name:"anshw pandey",
+    address:"sujakhar",
+    }
+]
+
+
+
+
+
+for(let i = 0 ; i < userProfile.length; i++)
+{
+    console.log(userProfile[i]);
+    
+}
+
+
