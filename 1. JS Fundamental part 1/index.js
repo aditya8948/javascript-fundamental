a3122350b4484197c40022f512349a4ce727ea20
aditya <89adityapandey@gmail.com>
2025-12-11 13:22:50 +0530

    updated index.js

--- 1. JS Fundamental part 1/index.js
+++ 1. JS Fundamental part 1/index.js
@@ -1,36 +1,40 @@
-// const userProfile = {
-//     name:"ansh pandey",
-//     address:"sujakhar",
-// }
+// let js = "amazing";
+//       if(js === "amazing")                    
+//         alert("javaScript is fun!");
+//       console.log(20+30);
 
 
-const userProfile = [
-    {
-    name:"ansh pandey",
-    address:"sujakhar",
-    },
-    {
-    name:"ansh  4pandey",
-    address:"sujakhar",
-    },
-    {
-    name:"ansh2 pandey",
-    address:"sujakhar",
-    },
-    {
-    name:"anshw pandey",
-    address:"sujakhar",
-    }
-]
+// ! primitive datatype in javascript 
 
 
-   
+/*
+Number => floating point number , age = 23;
+String => sequence of character , firstName = "rahul"
+Boolean => logical type that  can only be true or false 
+undefined => variable that is not yet  defined 
+NUll => empty value
+symbol => unique value that cant be change 
+bigInt => large integer than the nummber cant hold
+*/
+/*
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
 
+// typeof => use to check datatype
 
-for(let i = 0 ; i < userProfile.length; i++)
-{
-    console.log(userProfile[i]);
-    
-}
+console.log(typeof true); => boolean 
+console.log(typeof javaScriptIsFun); => boolean 
+console.log(typeof 23);           => number  
+console.log(typeof "hello");      => string
 
+let year ;
+console.log(year);      => undefined 
+console.log(typeof year); => undefined
+*/
 
+// ! let ,const , var
+/*
+let => mutable ,we can change value further 
+const => immutable , we cant change value after declaring
+var => mutable but it is function scoped and let is blocked scoped
+*/
