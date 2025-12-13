4cf28afad60a9b98f728cafa9f1d47f55dd52526
aditya <89adityapandey@gmail.com>
2025-12-13 22:43:15 +0530

    updated index.js

--- 1. JS Fundamental part 1/index.js
+++ 1. JS Fundamental part 1/index.js
@@ -38,3 +38,100 @@ let => mutable ,we can change value further
 const => immutable , we cant change value after declaring
 var => mutable but it is function scoped and let is blocked scoped
 */
+
+
+
+// !string and template literal 
+/*
+
+const firstName = "shubham ";
+const birthYear = 2001;
+const job = "teacher " ;
+
+// to concatinate
+
+const john = "i am " + firstName + "and i am a "
++ job;
+console.log(john);
+
+// easier way to do it 
+// using template literal
+
+const johnNew = `i am ${firstName} and i am a ${job}`;
+console.log(johnNew);
+
+console.log(`
+    this
+    is multi 
+    line code we can write 
+    with the help template `);
+
+*/
+
+// ! type conversion and type coerion
+
+// type conversion  syntax => datatype(value)
+let inputYear = '1991';
+console.log(typeof inputYear);  =>  string
+let convert = Number(inputYear);
+console.log(typeof convert);   => number
+
+// type coerion=> js automatically convert dt acrd to need 
+
+console.log('aditya' + 34); => aditya34
+console.log(3 + 34 + '5');  =>375
+
+
+// prompt is use to get any input
+
+const numb = prompt("enter the number");
+
+== (loose check ) 
+=== (strict check )   => datatype will also be checked
+!= (loose check )
+!== (strict check)
+
+
+// logical operator
+
+const hasLicense = true;
+let hasGoodVision = true ;
+
+console.log(hasGoodVision && hasLicense)  => true 
+ hasGoodVision = false  ;
+console.log(hasGoodVision || hasLicense)  => false 
+
+// !switch case 
+
+const day = "monday";
+
+switch(day){
+    case 'monday' : // day  === 'monday
+        console.log("busy");
+        break;
+    
+    case 'tuesday' :
+        console.log("go work");
+        break;
+    
+    case 'wednesday':
+    case 'thursday' :
+        console.log("sleep");
+
+    default :
+        console.log("ghar ja");
+        
+}
+
+// !ternary operator
+
+const age = 23;
+ 
+age > 18 ? console.log("ride bike ") : console.log("ride bicycle");
+
+const drink = age > 18 ? 'wine ' : 'water ';
+
+console.log(drink);  => wine 
+
+
+
