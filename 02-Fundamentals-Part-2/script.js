527b7f3ea710373f6d5fe38075aec5087e67accd
aditya <89adityapandey@gmail.com>
2025-12-15 00:21:17 +0530

    updated script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -4,4 +4,58 @@
 
 'use strict';   // we can use it by writing this line 
 
+/*
+// ! Function => a function in javascript is a block of code that perform specific task and runs when it is called 
 
+// function functionName(parameters) {
+//     // code to be executed
+// }
+
+// example 
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 3);
+console.log(result) ; // Output: 8
+
+// function declaration  => Defined using function with a name
+function calcAge(birthday) {
+    const age = 2037;
+    return age - birthday;
+}
+const age1 = calcAge(1991);
+
+// function expression => function stored in a variable
+
+const calcAge2 = function(birthyear){
+    return 2037 -birthyear
+}
+const age2 = calcAge2(1991);
+
+console.log(age1 , age2 );
+
+// ! arrow function => An arrow function is a shorter and cleaner way to write a function in JavaScript using =>.
+
+const functionName = (parameters) => {
+    // code to execute
+};
+
+
+// example
+
+// without writing return
+const addSum = (a, b) =>  a+b;
+
+console.log(addSum(4, 6)); // Output: 10
+
+// with return 
+const addNum = (a, b) => {
+     let jod = a + b;
+     return jod*a+b ;
+};
+
+console.log(addNum(4, 6)); // Output: 46
+
+*/
