9c7e0b6962b76469dc2f11b2518b6fbeb550477f
aditya <89adityapandey@gmail.com>
2025-12-14 23:30:45 +0530

    adding fundamental part 2 folder

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,7 @@
+// **“use strict”** is a JavaScript directive that **forces you to write cleaner and safer code by
+//  catching errors** (like using variables without declaring them).
+
+
+'use strict';   // we can use it by writing this line 
+
+
