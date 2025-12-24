ed3700e578d4de2dc5db50d2eca5afdbb43469cb
aditya <89adityapandey@gmail.com>
2025-12-24 23:38:00 +0530

    javaScript basic part2 over

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -60,6 +60,7 @@ console.log(addNum(4, 6)); // Output: 46
 
 */
 
+
 // !function call other function
 
 // function add(a, b) {
@@ -71,3 +72,83 @@ console.log(addNum(4, 6)); // Output: 46
 // }
 
 // console.log(calculateSum(5, 3)); // Output: 8
+
+
+//! array => An array in JavaScript is a special variable that can store multiple values in a single variable.
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits[0]); // Apple
+console.log(fruits[2]); // Mango
+
+// some method 
+// push => add at last 
+fruits.push("Guava");
+console.log(fruits);
+// [ 'Apple', 'Banana', 'Mango', 'Guava' ]
+
+
+//unshift => add element at first position 
+
+fruits.unshift("Grapes");
+console.log(fruits);
+// [ 'Grapes', 'Apple', 'Banana', 'Mango', 'Guava' ]
+
+// remove element 
+
+// pop => remove last element 
+
+fruits.pop() // return pop element 
+console.log(fruits); //=> [ 'Grapes', 'Apple', 'Banana', 'Mango' ]
+// shift => remove first element 
+
+fruits.shift();  // return first element 
+console.log(fruits); // => [ 'Apple', 'Banana', 'Mango' ]
+
+// include => return true if element is present in the array 
+
+console.log(fruits.includes('Banana')); // => true
+
+/////////////////////////////////////////////////////////
+
+// !object => An object in JavaScript is a collection of key–value pairs 
+// used to store related data and functions.
+
+let student = {
+    firstname: "Aditya",
+    age: 20,
+    course: "BTech",
+    isActive: true
+};
+
+console.log(student);  // => { name: 'Aditya', age: 20, course: 'BTech', isActive: true }
+
+console.log(student.firstname) // => aditya  
+console.log(student['first' + 'name']) // =>  aditya 
+
+const interested = student.age ;
+console.log(student[interested]) //=> 20;   
+
+// to add key and value  in object 
+student.location = 'India'; 
+student['insta' ] = 'ansh@'
+
+// this method => this refers to the object that is calling the method.
+
+const user = { 
+    name: 'rahul',
+    age: 20,
+    haveLicense: true,
+    greet : function(){
+        console.log(`hello ${this.name}`)
+    },
+
+    getSummary : function(){
+console.log(`hey my name is ${this.name} i am ${this.age} year old and i ${this.haveLicense ? 'have' : 'dont have'} license `)
+    }
+
+
+};
+
+user.greet(); // => hello rahul
+user.getSummary(); // => hey my name is rahul i am 20 year old and i have license 
